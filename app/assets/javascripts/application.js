8f4912d36eb8f3867b98533bb55284a08caaaf15
SaladRaider <peterkangveerman@gmail.com>
2016-05-08 14:34:34 -0700

    Created Video model and impletemented the video/new page, started implementing video/show page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,100 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require_tree .
+wow = new WOW(
+{
+    boxClass:     'wow',      // default
+    animateClass: 'animated', // default
+    offset:       50,          // default
+    mobile:       true,       // default
+    live:         true        // default
+});
+wow.init();
+
+$('.btn-see-down').click(function(){
+    $('html, body').animate(
+        {scrollTop: $('#page-content-start').offset().top - 48}, 
+        1000);
+})
+
+$(window).load(function(){
+    if($(window).width() < 768) {
+        $('.navbar-scroll').show();
+    }
+
+    if(!$('.navbar').length) {
+        $('.navbar-scroll').show();
+        $('body').css('padding-top','58px');
+    }
+    //$('#show-case-video').prop("volume","0.5");
+})
+
+$(window).resize(function(){
+    if($(window).width() < 768) {
+        $('.navbar-scroll').show();
+        $('.navbar-scroll').css("opacity", "1");
+        $('.navbar-scroll').css("top", "0");
+    } else {
+        if($('.collapseable-menu').is(":visible")) {
+            $('.collapseable-menu').hide();
+            $('.navbar-scroll').css("height", "48px");
+        }
+        if($('.navbar').length
+        && $(window).scrollTop() < 48*2
+        && $('.navbar-scroll').css("opacity") != "0"
+        && $(window).width() >= 768) {
+            $('.navbar-scroll').animate({
+                "opacity":"0",
+                "top":"-100px"},
+                500,
+                function(){$('.navbar-scroll').hide();}
+            );
+        }
+    }
+});
+
+// check to see if below nav bar 
+$(window).bind('scroll', function() {
+
+    /*if($(window).scrollTop() < $(window).height() - 48) {
+        $('.show-case-video').prop("volume", 
+            ($(window).height() - 48 - $(window).scrollTop()) / ($(window).height() - 48) / 2);
+    }
+    else  {
+        $('.show-case-video').prop("volume","0.0");
+    }*/
+
+    if($('.navbar-scroll').is(':animated')) {
+        return;
+    }
+    if($(window).scrollTop() > 48*4
+        && $('.navbar-scroll').css("opacity") == "0"
+        || $(window).width() < 768) {
+        $('.navbar-scroll').show();
+        $('.navbar-scroll').animate({
+            "opacity":"1",
+            "top":"0"},
+            500
+        );
+    } else if($('.navbar').length
+        && $(window).scrollTop() < 48*2
+        && $('.navbar-scroll').css("opacity") != "0"
+        && $(window).width() >= 768) {
+        $('.navbar-scroll').animate({
+            "opacity":"0",
+            "top":"-100px"},
+            500,
+            function(){$('.navbar-scroll').hide();}
+        );
+    }
+});
+
+$('.nav-ham-toggle').click(function() {
+    if(!$('.collapseable-menu').is(":visible")) {
+        $('.navbar-scroll').css("height", "auto");
+        $('.collapseable-menu').show(500);
+    } else {
+        
+        $('.collapseable-menu').hide(500,function(){
+            $('.navbar-scroll').css("height", "48px");
+        });
+    }
+    
+});
--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,4 +1,24 @@
 class VideosController < ApplicationController
 	def index
 	end
+
+	def new
+	end
+
+	def create
+		@video = Video.new(video_params)
+		@video.save
+
+		redirect_to @video
+	end
+
+	def show
+		@video = Video.find(params[:id])
+	end
+
+	private 
+		def video_params
+			params.require(:video).permit(:title, :show, :link, 
+				:description, :keywords, :category);
+		end
 end
