45418237574c19377ef9d2192d280f748f799044
SaladRaider <peterkangveerman@gmail.com>
2016-05-10 15:36:02 -0700

    localized dependencies, edited videos view, added static pages with high_voltage

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'devise', '~> 4.1'
 gem 'friendly_id', '~> 5.1.0'
+gem 'high_voltage', '~> 3.0.0'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   resources :videos, :users
-  root "videos#index"
+  get '/join' => 'high_voltage/pages#show', id: 'join'
+  get '/about' => 'high_voltage/pages#show', id: 'about'
+  get '/contact' => 'high_voltage/pages#show', id: 'contact'
 end
