c34ada9380fb51749a89269c9d39130539b0d93a
SaladRaider <peterkangveerman@gmail.com>
2016-05-19 11:58:08 -0700

    Allow users to use markup in their bios using RedCarpet

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,28 @@
 module ApplicationHelper
+	def markdown(text)
+		options = {
+			filter_html:         true,
+			hard_wrap:           true, 
+			prettify:            true,
+			link_attributes:     { rel: 'nofollow', target: "_blank" },
+			space_after_headers: true, 
+			fenced_code_blocks:  true
+		}
+
+		extensions = {
+			autolink:                     true,
+			underline:                    true,
+			superscript:                  true,
+			no_intra_emphasis:            true,
+			strikethrough:                true,
+			highlight:                    true,
+			quote:                        true,
+			disable_indented_code_blocks: true
+		}
+
+		renderer = Redcarpet::Render::HTML.new(options)
+		markdown = Redcarpet::Markdown.new(renderer, extensions)
+
+		markdown.render(text).html_safe
+	end
 end
