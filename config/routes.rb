1ef640da2b6599046ac0215c164ed33f09cd518a
SaladRaider <peterkangveerman@gmail.com>
2016-05-20 10:17:12 -0700

    Finished about page. Alloweed video descriptions to use markup using Redcarpet

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   resources :videos, :users, :assigned_jobs
   get '/join' => 'high_voltage/pages#show', id: 'join'
-  get '/about' => 'high_voltage/pages#show', id: 'about'
+  get '/about' => 'users#index', id: 'index'
   get '/contact' => 'high_voltage/pages#show', id: 'contact'
   get '/suggest' => 'high_voltage/pages#show', id: 'suggest'
 end
