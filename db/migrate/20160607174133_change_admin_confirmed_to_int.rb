97189835f27a3889b7b0111c2bd04e797c59ac67
SaladRaider <peterkangveerman@gmail.com>
2016-12-29 13:35:22 -0800

    Make migration compatible with pgsql

--- db/migrate/20160607174133_change_admin_confirmed_to_int.rb
+++ db/migrate/20160607174133_change_admin_confirmed_to_int.rb
@@ -1,5 +1,21 @@
 class ChangeAdminConfirmedToInt < ActiveRecord::Migration
-  def change
-  	change_column :users, :admin_confirmed, 'integer USING CASE admin_confirmed WHEN \'true\' THEN 1 ELSE 0 END'
+  def up
+  	add_column :users, :convert_admin_confirmed, :integer, :default => true
+
+    Project.all.each do |p|
+      if p.admin_confirmed
+        p.convert_admin_confirmed = 1
+      else
+        p.convert_admin_confirmed = 0
+      end
+    end
+
+    remove_column :users, :admin_confirmed
+    rename_column :users, :convert_admin_confirmed, :admin_confirmed
   end
+
+  def down
+    change_column :users, :admin_confirmed, :boolean
+  end
+
 end
