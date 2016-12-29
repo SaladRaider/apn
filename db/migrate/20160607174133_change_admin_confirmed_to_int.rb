3c2d396b32a8a4c12909313f3ecac7314796af3b
SaladRaider <peterkangveerman@gmail.com>
2016-12-29 13:05:38 -0800

    Use Using statement in migration.

--- db/migrate/20160607174133_change_admin_confirmed_to_int.rb
+++ db/migrate/20160607174133_change_admin_confirmed_to_int.rb
@@ -1,5 +1,5 @@
 class ChangeAdminConfirmedToInt < ActiveRecord::Migration
   def change
-  	change_column :users, :admin_confirmed, :integer
+  	change_column :users, :admin_confirmed, 'integer USING CAST(column_name AS integer)'
   end
 end
