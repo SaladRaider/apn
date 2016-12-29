ef193b0514d498f306f657f44215aa7e59524a2c
SaladRaider <peterkangveerman@gmail.com>
2016-12-29 13:21:10 -0800

    Remove default from migration.

--- db/migrate/20160607174133_change_admin_confirmed_to_int.rb
+++ db/migrate/20160607174133_change_admin_confirmed_to_int.rb
@@ -1,5 +1,5 @@
 class ChangeAdminConfirmedToInt < ActiveRecord::Migration
   def change
-  	change_column :users, :admin_confirmed, 'integer USING CASE admin_confirmed WHEN \'true\' THEN 1 ELSE 0 END', :default => 0
+  	change_column :users, :admin_confirmed, 'integer USING CASE admin_confirmed WHEN \'true\' THEN 1 ELSE 0 END'
   end
 end
