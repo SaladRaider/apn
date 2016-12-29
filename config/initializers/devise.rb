055bf007c5613def3e13f9be5741a68b5c878603
SaladRaider <peterkangveerman@gmail.com>
2016-12-29 15:39:42 -0800

    Add mail host.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,8 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'support@apn.tv'
+  config.mailer_sender = 'support@apachenewsapn.org'
+  config.action_mailer.default_url_options = { :host => 'www.apachenewsapn.org' }
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
